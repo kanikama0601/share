df9186a657634b59768fbf12d41c0e7ac1de5aa4
kanikama0601 <kanikama0601school@gmail.com>
2024-05-08 01:42:00 +0900

    commit

--- software_9_hint.c
+++ software_9_hint.c
@@ -3,8 +3,9 @@
 int main(void)
  
 {
-    int max = 20;
-    printf("高速化のススメ\nまず、すべて素数と考える\n");
+    int max = 26;
+    printf("高速化のヒント\n\n今回、範囲は1～%dまで\n\n",max);
+    printf("まず、すべて素数と考える\n");
     bool a[max+1]; //trueかfalseか
     for(int i = 1;i <= max;i++)
     {
@@ -55,6 +56,10 @@ int main(void)
 }
 
 /*実行結果
+高速化のヒント
+
+今回、範囲は1～26まで
+
 まず、すべて素数と考える
 1 = true
 2 = true
@@ -76,6 +81,12 @@ int main(void)
 18 = true
 19 = true
 20 = true
+21 = true
+22 = true
+23 = true
+24 = true
+25 = true
+26 = true
 もし素数じゃない場合、aをfalseにする
 
 まず、1を除外(1は素数ではない)
@@ -84,7 +95,7 @@ int main(void)
 
 次に、iが素数の時、iの倍数を全部falseにする
 
-1回目
+1回目 (i = 2、j = 4)
 1 = false
 2 = true
 3 = true
@@ -105,8 +116,14 @@ int main(void)
 18 = false
 19 = true
 20 = false
+21 = true
+22 = false
+23 = true
+24 = false
+25 = true
+26 = false
 
-2回目
+2回目 (i = 3、j = 9)
 1 = false
 2 = true
 3 = true
@@ -127,8 +144,14 @@ int main(void)
 18 = false
 19 = true
 20 = false
+21 = false
+22 = false
+23 = true
+24 = false
+25 = true
+26 = false
 
-4は素数じゃないので、スキップ
+4は素数じゃないので、スキップ (i = 4)
 1 = false
 2 = true
 3 = true
@@ -149,6 +172,40 @@ int main(void)
 18 = false
 19 = true
 20 = false
+21 = false
+22 = false
+23 = true
+24 = false
+25 = true
+26 = false
+
+3回目 (i = 5、j = 25)
+1 = false
+2 = true
+3 = true
+4 = false
+5 = true
+6 = false
+7 = true
+8 = false
+9 = false
+10 = false
+11 = true
+12 = false
+13 = true
+14 = false
+15 = false
+16 = false
+17 = true
+18 = false
+19 = true
+20 = false
+21 = false
+22 = false
+23 = true
+24 = false
+25 = false
+26 = false
 
 
 これで素数の表が完成する
