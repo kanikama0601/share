a222ec02cfd1d2afd6647146e50379a0eb5b3dfc
kanikama0601 <kanikama0601school@gmail.com>
2025-02-17 14:20:51 +0900

    auto commit

--- js2/q2-6.py
+++ js2/q2-6.py
@@ -5,5 +5,12 @@ import matplotlib.pyplot as plt
 plt.scatter(x_data,y_data)
 plt.xlabel('月・年')
 plt.ylabel('最深積雪')
-plt.ylim(0, deepest_snowfall_12['最深積雪'].astype(int).max()+10)
 plt.show()
+
+# このままではグラフの日本語が文字化けするので、
+# 別のコードに
+# !pip install japanize_matplotlib
+# これを入れ実行した後、
+
+# import japanize_matplotlib
+# これを入れると文字化けが解消されます
